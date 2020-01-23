989e7fbf6c07b474d3fa8783efe3af4d32df1c68
Chelsea Kramer <chelsea.kramer99@gmail.com>
2020-01-22 19:36:25 -0500

    handoff

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
   return (
     <div>
 
-      <div class="slidecontainer">
+      <div className="slidecontainer">
         <p>Color slider:</p>
         <input type="range" min="1" max="100" value="50"></input>
+      </div>
+      <div className="slidecontainer">
+        <p>Color slider:</p>
+        <input type="range" min="1" max="100" value="50"></input>
+      </div>
+      <div className="slidecontainer">
+        <p>Color slider:</p>
+        <input type="range" min="1" max="100" value="50"></input>
+      </div>
+      <div className="square">
+
       </div>
 
 
